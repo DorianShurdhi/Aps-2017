2080ce069ebfa67cea5b7866e1766261c5eff89b
Dorian Shurdhi <dorianshurdhi@hotmail.it>
2017-05-14 15:56:06 +0200

    ...

--- Aps-PlayMusic/src/it/uniroma3/aps/playmusic/AcmePlayMusic.java
+++ Aps-PlayMusic/src/it/uniroma3/aps/playmusic/AcmePlayMusic.java
@@ -2,11 +2,21 @@ package it.uniroma3.aps.playmusic;
 
 public class AcmePlayMusic {
 	private Album albumCorrente;
-	private Playlist playlistcorrente;
+	private Playlist playlistCorrente;
 	
+	
+
 	public void inserisciBranoNellaPlaylist(int posizione,String commento){
 		Brano b=this.albumCorrente.getBrano(posizione);
-		this.playlistcorrente.creaRiga(b,commento);
-		
+		this.playlistCorrente.creaRiga(b,commento);
 	}
+	public void confermaPlaylist(String titolo,String descrizione){
+
+	}
+	
+	public void annullaInserimento(){
+		this.playlistCorrente=null;
+	}
+
+
 }
